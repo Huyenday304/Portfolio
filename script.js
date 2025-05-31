3ec5dd215d4484807336c2350bff9cc225d58348
Huyenday304 <ngnghuyen0403@gmail.com>
2025-05-31 13:49:37 +0700

    update 31/5

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+
 function toggleMenu() {
-    const menu = document.getElementById('menu-mobile');
-    menu.classList.toggle('show-menu');
-    
+  const menu = document.getElementById('menu-mobile');
+  // const toggle = document.getElementById('menuToggle');
+  const body = document.body;
+  const nav = document.querySelector('nav');
+
+  menu.classList.toggle('show-menu');
+  nav.classList.toggle('menu-open');
+  // toggle.classList.toggle('active');
+  if (menu.classList.contains('show-menu')) {
+    body.classList.add('no-scroll');
+  } else {
+    body.classList.remove('no-scroll');
+  }
+
 }
+
 
 window.addEventListener('load', function() {
     const preloader = document.getElementById('preloader');
